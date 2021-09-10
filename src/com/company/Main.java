b11d90db7312c0b1872e63ce7920f4e1e20fea2e
Dnguyen99 <dylannguyen@Knights.ucf.edu>
2021-09-10 12:14:48 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the length of the room in feet? ");
+        int length = input.nextInt();
+        System.out.println("What is the width of the room in feet?" );
+        int width = input.nextInt();
+        System.out.println("You entered dimensions of " +length+ " by " +width+ ".");
+
+        int area = length * width;
+        System.out.println("The area is \n" +area+ " feet");
+
+        double conversionFactor = area * 0.09290304;
+        System.out.println(String.format("%.3f", conversionFactor)+ " square meters.");
+    }
+}
